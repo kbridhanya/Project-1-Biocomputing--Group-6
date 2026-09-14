04b56971edd5c4c55c8130140610a3a1d8aa8d51
kbridhanya <kbridhanya@iitgn.ac.in>
2026-09-14 15:02:08 +0000

    Initial project

---
+++ table.py
@@ -0,0 +1,4 @@
+print("Gene Name | Category | CDS Length | Translated Length | Deposited Length | Identical | Position of Mismatch")
+print("MSRB1 | A | 351 | 116| 116 | No | 95")
+print("MT-ND1 | B | 956 | 318 | 318 | Yes | No")
+print("ACTB | C | 1128 | 375 | 375 | Yes | No")
